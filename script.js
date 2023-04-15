5e17fc8ad66f4d2cde33a7b45889ae9bdcb6d04e
Liam Forsey <98509939+lforsey@users.noreply.github.com>
2023-04-14 20:29:02 -0700

    20 minute fizzbuzz solve

---
+++ script.js
@@ -0,0 +1,32 @@
+const btn = document.querySelector("#inputBtn");
+const input = document.querySelector('#userInput');
+const form = document.querySelector('#inputForm');
+
+const fizzBuzz = (num) => {
+    let arr = [];
+    while (num > 0) {
+        if (num % 3 === 0 && num % 5 === 0) {
+            arr.push('FizzBuzz');
+            num --;
+        } else if (num % 3 === 0) {
+            arr.push('Fizz');
+            num --;
+        } else if (num % 5 ===0) {
+            arr.push('Buzz');
+            num --;
+        } else {
+            arr.push(num);
+            num --;
+        }
+    }     
+    const newDiv = document.createElement('div');
+    const answer = document.createTextNode(arr)
+    newDiv.appendChild(answer);
+    form.appendChild(newDiv);
+}
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const inputNum = input.value;
+    fizzBuzz(inputNum);
+});
